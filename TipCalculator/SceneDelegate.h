7abd5cb28aa0f59fb382885c94cd2bb7df504db7
Abdiel Cortes <zurken@fb.com>
2020-06-24 14:45:05 -0700

    Initial Commit

---
+++ TipCalculator/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  TipCalculator
+//
+//  Created by zurken on 6/24/20.
+//  Copyright © 2020 FacebookUniversity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
