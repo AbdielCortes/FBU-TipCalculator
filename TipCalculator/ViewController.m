9df53d6e70ea69d563b570c92aae292b66391e25
Abdiel Cortes <zurken@fb.com>
2020-06-24 20:44:44 -0700

    Implemented bill fields, buttons & calc

--- TipCalculator/ViewController.m
+++ TipCalculator/ViewController.m
@@ -10,6 +10,14 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *billField;
+
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
+
 @end
 
 @implementation ViewController
@@ -19,13 +27,23 @@
     // Do any additional setup after loading the view.
 }
 
+// closes keyboard when we tap the screen
 - (IBAction)onTap:(id)sender {
     [self.view endEditing:YES];
     NSLog(@"Closed keyboard");
 }
 
+// manages input box
 - (IBAction)onEdit:(id)sender {
+    NSArray *percentages = @[@(0.15), @(0.20), @(0.22)];
     
+    double bill = [self.billField.text doubleValue];
+    double tipPercentage = [percentages[self.tipControl.selectedSegmentIndex] doubleValue];
+    double tip = tipPercentage * bill;
+    double total = bill + tip;
+    
+    self.tipLabel.text = [NSString stringWithFormat:@"$%.2f", tip];
+    self.totalLabel.text = [NSString stringWithFormat:@"$%.2f", total];
 }
 
 @end
